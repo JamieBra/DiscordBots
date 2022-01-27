3719f60a4a19c2be3f806afdd5bf70a355e13cd9
ItsPonks <34966414+ItsPonks@users.noreply.github.com>
2022-01-26 21:17:00 -0500

    Fix failing to send content and add more information to message

--- scheduler.py
+++ scheduler.py
@@ -1,22 +1,29 @@
 from asyncio import create_task, sleep
 from datetime import datetime
 from dateutil.parser import parse
-from hikari import TextableGuildChannel
+from hikari import ButtonStyle, TextableGuildChannel
 from lightbulb import owner_only
 from utils import SlashBot
 
-bot = SlashBot(owner_only)
+bot = SlashBot()
 
-async def coro(context, channel, time):
-    await sleep((time - datetime.now()).total_seconds())
+async def coro(context, channel, delay, content):
+    await sleep(delay.total_seconds())
     await channel.send(content)
     await context.delete_last_response()
 
-@bot.slash('Schedules a message to be sent... eventually.')
+@bot.slash('Schedules a message to be sent... eventually.', owner_only)
 async def schedule(context, channel: ('Where?', TextableGuildChannel), timestr: ('When?', str), content: ('What?', str)):
     channel = context.get_guild().get_channel(channel)
-    time = parse(timestr)
-    task = create_task(coro(context, channel, time))
-    await context.respond(f'Scheduled `{content}` to be sent in {channel.mention} at __{time}__.', component=bot.button(task.get_name(), 'Cancel', task.cancel))
+    time = parse(timestr, fuzzy=True)
+    delay = time - datetime.now()
+    task = create_task(coro(context, channel, delay, content))
+    
+    @bot.button(task.get_name(), 'Cancel', ButtonStyle.DANGER)
+    async def cancel(_):
+        task.cancel()
+        await context.delete_last_response()
+    
+    await context.respond(f'Scheduled `{content}` to be sent in {channel.mention} at __{time}__ ({delay}).', component=cancel)
 
 bot.run()
