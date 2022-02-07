1a5fa03a4de1b91ae7733cd0a75871d348284288
ItsPonks <34966414+ItsPonks@users.noreply.github.com>
2022-02-07 02:59:09 -0500

    Replace for loops with timeouts

--- quotatron.py
+++ quotatron.py
@@ -1,7 +1,9 @@
-from datetime import datetime, timezone
-from hikari import ButtonStyle, CacheSettings, Member
+from app import SlashBot
+from async_timeout import timeout
+from asyncio import TimeoutError
+from datetime import datetime, timedelta, timezone
+from hikari import ButtonStyle, CacheSettings, Member, UNDEFINED
 from random import choice, sample, uniform
-from utils import SlashBot
 
 bot = SlashBot(cache_settings=CacheSettings(max_messages=10000))
 
@@ -11,29 +13,32 @@ async def find(context, success, failure, *members):
     link = None
     a = datetime.now(timezone.utc)
 
-    for _ in range(5):
-        content = ''
-        messages = set()
-        for member in members:
-            predicates = [lambda m: m.content and not (m.mentions.everyone or m.mentions.role_ids or m.mentions.users) and m.content not in messages and '://' not in m.content]
-            attrs = dict(attachments=[], embeds=[], stickers=[])
-            if member:
-                b = max(channel.created_at, member.joined_at)
-                attrs['author'] = member.user
-            else:
-                b = channel.created_at
-                predicates.append(lambda m: not m.author.is_bot and m.author.discriminator != '0000')
+    try:
+        async with timeout(900) as to:
+            content = ''
+            messages = set()
+            for i, member in enumerate(members):
+                predicates = [lambda m: m.content and not (m.mentions.everyone or m.mentions.role_ids or m.mentions.users) and m.content not in messages and '://' not in m.content]
+                attrs = dict(attachments=[], embeds=[], stickers=[])
+                if member:
+                    b = max(channel.created_at, member.joined_at)
+                    attrs['author'] = member.user
+                else:
+                    b = channel.created_at
+                    predicates.append(lambda m: not m.author.is_bot and m.author.discriminator != '0000')
 
-            for _ in range(50):
-                if history := await channel.fetch_history(around=uniform(a, b)).limit(101).filter(*predicates, **attrs):
-                    m = choice(history)
-                    link = m.make_link(guild)
-                    messages.add(m)
-                    content += success.format(username=m.author.username, content=m.content.replace('\n', ' \\ '), date=m.timestamp.date())
-                    break
-        if len(content) <= 2000:
-            return content or failure, link
-    return 'All attempts at finding quotes exceeded the maximum length.', None
+                until = timedelta(seconds=(to.deadline - to._loop.time()) / (len(members) - i)) + datetime.now()
+                while datetime.now() < until:
+                    if history := await channel.fetch_history(around=uniform(a, b)).limit(101).filter(*predicates, **attrs):
+                        m = choice(history)
+                        link = m.make_link(guild)
+                        messages.add(m)
+                        content += success.format(username=m.author.username, content=m.content.replace('\n', ' \\ '), date=m.timestamp.date())
+                        break
+            if len(content) <= 2000:
+                return content or failure, link
+    except TimeoutError:
+        return 'All attempts at finding quotes exceeded the maximum length.', None
 
 @bot.slash('Randomly quotes members in this channel.')
 async def convo(context, *members: ('Quote whom?', Member), count: ('How many?', int) = 5):
@@ -43,6 +48,6 @@ async def convo(context, *members: ('Quote whom?', Member), count: ('How many?',
 @bot.slash('Randomly quotes a member in this channel.')
 async def quote(context, member: ('Quote whom?', Member) = None):
     content, link = await find(context, '"{content}" -{username}, {date}', 'No message found.', member)
-    await context.respond(content, component=bot.button('Original', ButtonStyle.LINK, link))
+    await context.respond(content, component=bot.button('Original', ButtonStyle.LINK, link) if link else UNDEFINED)
 
 bot.run()
