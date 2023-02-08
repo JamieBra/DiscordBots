f1e6d00dea8ef801d5f5b3dee5d9a2ad5c55df41
Jamie Brassel <jbras3@gmail.com>
2023-02-08 04:32:06 +0000

    rewrite a large portion of the command logic with miru and a more flexible/convenient vararg

--- quotatron.py
+++ quotatron.py
@@ -1,100 +1,102 @@
 from collections import Counter
-from datetime import datetime, timedelta, timezone
-from getpass import getpass
+from datetime import datetime, timezone
+from inspect import Parameter, signature
 from os import environ
-from random import choice, shuffle, uniform
+from random import choice, sample, uniform
 from re import compile
-from sys import maxsize
-from typing import Optional
+from typing import Annotated, get_args
 
-from crescent import ClassCommandProto, Client, Context, command, option
-from hikari import ButtonStyle, GatewayBot, Message, TextableChannel, User
-from hikari.impl import CacheSettings
-from uvloop import install
+import miru
+import uvloop
+from async_timeout import timeout
+from crescent import (ClassCommandOption, ClassCommandProto, Client, Context,
+                      command, option)
+from hikari import GatewayBot, Message, TextableGuildChannel, User
+from miru import Button, View
+from more_itertools import ilen
 
 
-def utcnow():
-    return datetime.now(timezone.utc)
-
-
-async def find(ctx: Context, success: str, *users: Optional[User]):
+async def find(ctx: Context, success: str, *users: User | None):
     def author(message: Message):
         return specific_users and message.author.id
 
-    def predicate(message: Message):
-        return not (message.content is None or message.author.discriminator == '0000' or message in messages or pattern.search(message.content)) and author(message) in counts
-
-    if isinstance(ctx.channel, TextableChannel):
-        start = utcnow()
-        deadline = start + timedelta(minutes=15)
-
+    async with timeout(900):
         content = ''
         link = None
-
         messages: set[Message] = set()
-        counts = Counter(user and user.id for user in users if not (user and user.is_bot))
-        specific_users = users[0]
 
-        while counts and utcnow() < deadline:
-            if history := await ctx.channel.fetch_history(around=uniform(ctx.channel.created_at, start)).limit(101).filter(  # type: ignore
-                predicate,
-                ('author.is_bot', False),
-                mentions_everyone=False,
-                role_mention_ids=[],
-                user_mentions_ids=[],
-            ):
-                message = choice(history)
-                link = message.make_link(ctx.guild)
-                content += success.format(
-                    username=message.author.username,
-                    content=message.content,
-                    date=message.timestamp.date()
-                )
-                messages.add(message)
-                counts[author(message)] -= 1
-                counts = +counts
+        if isinstance(ctx.channel, TextableGuildChannel):
+            counts = Counter(user and user.id for user in users if not (user and user.is_bot))
+            now = datetime.now(timezone.utc)
+            specific_users = users[0]
+
+            while counts:
+                if history := await ctx.channel.fetch_history(around=uniform(ctx.channel.created_at, now)).limit(101).filter(  # type: ignore
+                    lambda m: bool(m.content) and m.author.discriminator != '0000' and m not in messages and not pattern.search(m.content) and author(m) in counts,  # type: ignore
+                    ('author.is_bot', False),
+                    mentions_everyone=False,
+                    role_mention_ids=[],
+                    user_mentions_ids=[],
+                ):
+                    message = choice(history)
+                    link = message.make_link(ctx.guild)
+                    content += success.format(
+                        username=message.author.username,
+                        content=message.content,
+                        date=message.timestamp.date()
+                    )
+                    messages.add(message)
+                    counts[author(message)] -= 1
+                    counts = +counts
+
         if content and len(content) <= 2000:
             return content, link
-    return 'No messages found.', None
+        return 'No messages found.', None
 
 
-bot = GatewayBot(environ.get('DISCORD') or getpass(), cache_settings=CacheSettings(max_messages=maxsize))
-client = Client(bot, command_hooks=[Context.defer])
+def parse_annotation(clazz: type[ClassCommandProto]):
+    callback = clazz.callback
+    clazz.callback = lambda self, ctx: callback(self, ctx, *filter(None, map(ctx.options.get, variadic)))
+
+    options = 26 - ilen(filter(ClassCommandOption.__instancecheck__, clazz.__dict__.values()))
+    variadic: list[str] = []
+    for param in signature(callback).parameters.values():
+        if param.kind is Parameter.VAR_POSITIONAL:
+            for i in range(1, options):
+                name = f'{param.name}{i}'
+                variadic.append(name)
+                setattr(clazz, name, option(*get_args(param.annotation), default=None))
+            break
+
+    return clazz
+
+
+bot = GatewayBot(environ.get('DISCORD', ''))
+client = Client(bot, default_guild=581657201123262491, command_hooks=[Context.defer])
 pattern = compile(r'\n|://')
-
-
-def add_users(callback: type[ClassCommandProto]):
-    for i in range(1, 25):
-        setattr(callback, f'user{i}', option(User, default=None))
-    return callback
 
 
 @client.include
 @command(name='convo')
-@add_users
+@parse_annotation
 class Convo:
-    count = option(int, default=5, min_value=2, max_value=25)
+    count = option(int, 'How many?', default=5, min_value=2, max_value=25)
 
-    async def callback(self, ctx: Context):
-        users = [v for k, v in ctx.options.items() if k != 'count']
-        shuffle(users)
-        content, _ = await find(ctx, '{username}: {content}\n', *users or [None] * self.count)
+    async def callback(self, ctx: Context, *users: Annotated[User, 'Quote whom?']):
+        content, _ = await find(ctx, '{username}: {content}\n', *sample(users, len(users)) or (None,) * self.count)
         await ctx.respond(content)
 
 
 @client.include
-@command
-async def quote(ctx: Context, user: Optional[User] = None):
+@command(name='quote')
+async def quote(ctx: Context, user: Annotated[User | None, 'Quote whom?'] = None):
     content, link = await find(ctx, '"{content}" -{username}, {date}', user)
-
+    view = View()
     if link:
-        await ctx.respond(
-            content,
-            component=bot.rest.build_message_action_row().add_button(ButtonStyle.LINK, link).set_label('Original').add_to_container()
-        )
-    else:
-        await ctx.respond(content)
+        view.add_item(Button(label='Original', url=link))
+    await ctx.respond(content, components=view)
 
 
-install()
+miru.install(bot)
+uvloop.install()
 bot.run(check_for_updates=False)
